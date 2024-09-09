8e9ed8acc1283b228f92e4e4968707fc5b607d4a
Ali Sheharyar <ali.sheharyar@fasset.com>
2024-09-09 13:08:06 +0500

    Remittance API cron (#24)

---
+++ locals_remittance_api.tf
@@ -0,0 +1,57 @@
+data "aws_secretsmanager_secret_version" "sm_remittance_api" {
+  secret_id = "${local.environment_stage}-secret-remittance-api"
+}
+
+data "aws_ssm_parameter" "ssm_remittance_api" {
+  name = "/${local.environment_stage}/general/remittance-api/REPOSITORY_URL"
+}
+
+locals {
+  dynamic_schedulers_remittance_api = {
+
+    dev = {
+      updateTransactionStatusCron = {
+        cron_expressions = "cron(*/15 * * * *)"
+        payload = {
+          url    = data.aws_ssm_parameter.ssm_remittance_api.value
+          path   = "/api/v1/remittance/updateTransactionStatusCron"
+          method = "GET"
+          header = {
+            x-api-key = jsondecode(data.aws_secretsmanager_secret_version.sm_remittance_api.secret_string)["REMITTANCE_API_KEY"]
+          }
+        }
+      }
+    }
+
+    # stg = {
+    #   updateTransactionStatusCron = {
+    #     cron_expressions = "cron(*/10 * * * *)"
+    #     payload = {
+    #       url    = data.aws_ssm_parameter.ssm_remittance_api.value
+    #       path   = "/api/v1/remittance/updateTransactionStatusCron"
+    #       method = "GET"
+    #       header = {
+    #         x-api-key = jsondecode(data.aws_secretsmanager_secret_version.sm_remittance_api.secret_string)["REMITTANCE_API_KEY"]
+    #       }
+    #     }
+    #   }
+    # }
+
+    # prod = {
+    #   updateTransactionStatusCron = {
+    #     cron_expressions = "cron(*/10 * * * *)"
+    #     payload = {
+    #       url    = data.aws_ssm_parameter.ssm_remittance_api.value
+    #       path   = "/api/v1/remittance/updateTransactionStatusCron"
+    #       method = "GET"
+    #       header = {
+    #         x-api-key = jsondecode(data.aws_secretsmanager_secret_version.sm_remittance_api.secret_string)["REMITTANCE_API_KEY"]
+    #       }
+    #     }
+    #   }
+    # }
+
+  }
+
+  schedulers_remittance_api  = local.dynamic_schedulers_remittance_api[local.environment_stage]
+}
