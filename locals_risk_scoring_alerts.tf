af03caf74abff40c7f85ee3245391117726f1555
its-imranBashir <imran.bashir@fasset.com>
2024-11-01 23:26:31 +0500

    risk-scoring-alerts

---
+++ locals_risk_scoring_alerts.tf
@@ -0,0 +1,67 @@
+data "aws_secretsmanager_secret_version" "sm_cash_app_api" {
+  secret_id = "${local.environment_stage}-secret-cash-app-api"
+}
+
+data "aws_ssm_parameter" "ssm_cash_app_api" {
+  name = "/${local.environment_stage}/general/cash-app-api/REPOSITORY_URL"
+}
+
+locals {
+  risk_score_alerts_schedular = {
+    dev = {
+      riskScoringReminderAlerts = {
+        cron_expressions = "cron(0 0 * * ? *)"
+        payload = {
+          url    = data.aws_ssm_parameter.ssm_cash_app_api.value
+          path   = "/api/v1/users/riskScoringReminderAlerts"  
+          method = "POST"
+          header = {
+            token = jsondecode(data.aws_secretsmanager_secret_version.sm_cash_app_api.secret_string)["AWS_RISK_SCORE_CALC_SECRETS"]
+          }
+        }
+      }
+
+      riskScoringReminderAlertsTest = {
+        cron_expressions = "rate(10 minutes)"
+        payload = {
+          url    = data.aws_ssm_parameter.ssm_cash_app_api.value
+          path   = "/api/v1/users/riskScoringReminderAlerts"  
+          method = "POST"
+          header = {
+            token = jsondecode(data.aws_secretsmanager_secret_version.sm_cash_app_api.secret_string)["AWS_RISK_SCORE_CALC_SECRETS"]
+          }
+        }
+      }
+    }
+
+    stg = {
+      riskScoringReminderAlerts = {
+        cron_expressions = "cron(0 0 * * ? *)"
+        payload = {
+          url    = data.aws_ssm_parameter.ssm_cash_app_api.value
+          path   = "/api/v1/users/riskScoringReminderAlerts"
+          method = "POST"
+          header = {
+            token = jsondecode(data.aws_secretsmanager_secret_version.sm_cash_app_api.secret_string)["AWS_RISK_SCORE_CALC_SECRETS"]
+          }
+        }
+      }
+    }
+
+    prod = {
+      riskScoringReminderAlerts = {
+        cron_expressions = "cron(0 0 * * ? *)"
+        payload = {
+          url    = data.aws_ssm_parameter.ssm_cash_app_api.value
+          path   = "/api/v1/users/riskScoringReminderAlerts"
+          method = "POST"
+          header = {
+            token = jsondecode(data.aws_secretsmanager_secret_version.sm_cash_app_api.secret_string)["AWS_RISK_SCORE_CALC_SECRETS"]
+          }
+        }
+      }
+    }
+  }
+
+  scheduler_risk_scoring_alerts  = local.risk_score_alerts_schedular[local.environment_stage]
+}
---
+++ main_risk_scoring_alerts.tf
@@ -0,0 +1,22 @@
+resource "aws_scheduler_schedule" "cron_risk_scoring_alerts" {
+  for_each   = local.scheduler_risk_scoring_alerts
+  name       = "${local.environment_stage}_${each.key}"
+  group_name = "default"
+
+  flexible_time_window {
+    mode                      = "OFF"
+  }
+
+  schedule_expression          = each.value.cron_expressions
+  schedule_expression_timezone = "Asia/Jakarta"
+
+  target {
+    arn      = "arn:aws:lambda:ap-southeast-3:${data.aws_caller_identity.this.account_id}:function:${local.environment_stage}-cron-api-call"
+    role_arn = "arn:aws:iam::${data.aws_caller_identity.this.account_id}:role/${local.environment_stage}-ECSTaskExecutionRole-role"
+    input = jsonencode(each.value.payload)
+
+    retry_policy {
+      maximum_retry_attempts = 0
+    }
+  }
+}
